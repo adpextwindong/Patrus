224516b18e3e3f5648766443c4caf6fb3dcdac79
Takumi Crary <tcrary@gmail.com>
2021-11-29 00:43:21 +0900

    Split evalBop out and reduce boilerplate noise

--- src/Patrus/AST.hs
+++ src/Patrus/AST.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DoAndIfThenElse #-}
 module Patrus.AST where
 
 import Debug.Trace
@@ -37,44 +38,48 @@ plusTyMismatch = BOPTyMismatch "Operands must be two numbers or two strings."
 -- All trivial patterns where we can perform the operator are done here.
 -- Type checking and subexpression handling is done elsewhere.
 eval :: Expr -> Either Error Expr
-eval e@(Lit _) = Right e
+eval e@(Lit _) = return e
 eval (Group e) = eval e
 eval (UOp Negate e) = case eval e of
                         Right (Lit (NumberLit x)) -> Right $ Lit $ NumberLit (-x)
                         _ -> Left $ UOPTyMismatch "Operand must be a number."
 --Dispatch to truthy evals
 eval (UOp Not e) = evalNotTruthy e
-eval (BOp (Cmp EQ) e1 e2) = evalTruthy EQ (eval e1) (eval e2)
-eval (BOp (Cmp NEQ) e1 e2) = evalTruthy NEQ (eval e1) (eval e2)
+eval (BOp (Cmp EQ) e1 e2) = evalTruthy EQ e1 e2
+eval (BOp (Cmp NEQ) e1 e2) = evalTruthy NEQ e1 e2
+
 --Strict Type Matching operators
-eval (BOp (Cmp LT)  (Lit (BoolLit a)) (Lit (BoolLit b))) = Right $ Lit $ BoolLit $ a < b
-eval (BOp (Cmp LTE) (Lit (BoolLit a)) (Lit (BoolLit b))) = Right $ Lit $ BoolLit $ a <= b
-eval (BOp (Cmp GT)  (Lit (BoolLit a)) (Lit (BoolLit b))) = Right $ Lit $ BoolLit $ a > b
-eval (BOp (Cmp GTE) (Lit (BoolLit a)) (Lit (BoolLit b))) = Right $ Lit $ BoolLit $ a >= b
-
-eval (BOp Plus (Lit (StringLit s1)) (Lit (StringLit s2))) = Right $ Lit $ StringLit (s1 <> s2)
-eval (BOp Plus (Lit (NumberLit x)) (Lit (NumberLit y)))   = Right $ Lit $ NumberLit (x + y)
-eval (BOp Minus (Lit (NumberLit x)) (Lit (NumberLit y)))  = Right $ Lit $ NumberLit (x - y)
-eval (BOp Mul (Lit (NumberLit x)) (Lit (NumberLit y)))    = Right $ Lit $ NumberLit (x * y)
-eval (BOp Div (Lit (NumberLit x)) (Lit (NumberLit y)))    = Right $ Lit $ NumberLit (x / y)
-
 -- Sub expressions need to be evaluated and type mismatches should be handled.
-eval (BOp operator e1 e2) = case literalBopTyMatch (eval e1) (eval e2) of
-                                Left _ -> case operator of
-                                            Plus -> Left plusTyMismatch
-                                            _ ->    Left bopTyMismatch
+eval (BOp operator e1 e2) = do
+    (e1,e2) <- literalBopTyMatch operator e1 e2
+    return $ evalBop operator e1 e2
 
-                                Right (e1',e2') -> eval (BOp operator e1' e2')
+evalBop :: BinOp -> Expr -> Expr -> Expr
+evalBop (Cmp LT)  (Lit (BoolLit a)) (Lit (BoolLit b)) = Lit $ BoolLit $ a < b
+evalBop (Cmp LTE) (Lit (BoolLit a)) (Lit (BoolLit b)) = Lit $ BoolLit $ a <= b
+evalBop (Cmp GT)  (Lit (BoolLit a)) (Lit (BoolLit b)) = Lit $ BoolLit $ a > b
+evalBop (Cmp GTE) (Lit (BoolLit a)) (Lit (BoolLit b)) = Lit $ BoolLit $ a >= b
 
---eval e = trace ("EXHAUST: "<> show e) $ Left undefined
+evalBop Plus (Lit (StringLit s1)) (Lit (StringLit s2)) = Lit $ StringLit (s1 <> s2)
+evalBop Plus (Lit (NumberLit x)) (Lit (NumberLit y))   = Lit $ NumberLit (x + y)
+evalBop Minus (Lit (NumberLit x)) (Lit (NumberLit y))  = Lit $ NumberLit (x - y)
+evalBop Mul (Lit (NumberLit x)) (Lit (NumberLit y))    = Lit $ NumberLit (x * y)
+evalBop Div (Lit (NumberLit x)) (Lit (NumberLit y))    = Lit $ NumberLit (x / y)
+
+--evalBop op e1 e2 = trace ("EXHAUST: "<> show op <> " " <> show e1 <> " " <> show e2) $ undefined
 
 -- | Performs strict type matching for PLUS/MINUS/DIV/MUL/LT/LTE/GT/GTE
 -- Nil in any operand causes an error, EQ and NEQ should be handled earlier.
-literalBopTyMatch :: Either Error Expr -> Either Error Expr -> Either Error (Expr,Expr)
-literalBopTyMatch (Left err) _ = Left err
-literalBopTyMatch _ (Left err) = Left err
-literalBopTyMatch (Right e1) (Right e2) | sameLitType e1 e2 = Right (e1,e2)
-                                        | otherwise = Left bopTyMismatch
+literalBopTyMatch :: BinOp -> Expr -> Expr -> Either Error (Expr,Expr)
+literalBopTyMatch operator e1 e2 = do
+    e1 <- eval e1
+    e2 <- eval e2
+    if not $ sameLitType e1 e2
+    then case operator of
+            Plus -> Left plusTyMismatch
+            _    -> Left bopTyMismatch
+    else
+        return (e1,e2)
 
 sameLitType :: Expr -> Expr -> Bool
 sameLitType (Lit (NumberLit _)) (Lit (NumberLit _)) = True
@@ -82,11 +87,13 @@ sameLitType (Lit (StringLit _)) (Lit (StringLit _)) = True
 sameLitType (Lit (BoolLit _)) (Lit (BoolLit _)) = True
 sameLitType _ _ = False
 
-evalTruthy :: ComparrisonOp -> Either Error Expr -> Either Error Expr -> Either Error Expr
-evalTruthy _ err@(Left _) _ = err
-evalTruthy _ _ err@(Left _) = err
-evalTruthy EQ (Right (Lit a)) (Right (Lit b)) = Right $ Lit $ BoolLit $ literalTruth a == literalTruth b
-evalTruthy NEQ (Right (Lit a)) (Right (Lit b)) = Right $ Lit $ BoolLit $ literalTruth a /= literalTruth b
+evalTruthy :: ComparrisonOp -> Expr -> Expr -> Either Error Expr
+evalTruthy operator e1 e2 = do
+   e1 <- eval e1
+   e2 <- eval e2
+   return $ case operator of
+        EQ -> Lit $ BoolLit $ literalTruth e1 == literalTruth e2
+        NEQ -> Lit $ BoolLit $ literalTruth e1 /= literalTruth e2
 
 evalNotTruthy :: Expr -> Either Error Expr
 evalNotTruthy e = case eval e of
@@ -95,7 +102,7 @@ evalNotTruthy e = case eval e of
                     Right (Lit Nil) -> Right $ Lit $ BoolLit True               --Nil is falsy
                     Right e -> Right $ Lit $ BoolLit False                      --Every else is truthy
 
-literalTruth :: Literal -> Bool
-literalTruth Nil = False
-literalTruth (BoolLit False) = False
+literalTruth :: Expr -> Bool
+literalTruth (Lit Nil) = False
+literalTruth (Lit (BoolLit False)) = False
 literalTruth _ = True

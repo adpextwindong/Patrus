31517defb038d68fe73b16b6ed07a6bc14af5c40
Takumi Crary <tcrary@gmail.com>
2022-02-05 22:43:37 +0900

    Add first evalK test via indirectly checking env after assignment

--- test/Patrus/EvaluationSpec.hs
+++ test/Patrus/EvaluationSpec.hs
@@ -9,14 +9,19 @@ import Test.QuickCheck
 import Patrus.EvalK
 import Data.IORef
 import Control.Applicative (Alternative(empty))
+import qualified Data.Map as M
 
--- clown shit, does not work. Just test directly against the environment
--- spec :: Spec
--- spec = do
---     describe "Testing the continuation" $ do
---         it "successfully sets the IORef so we can test evalK" $ do
---             tref <- newIORef $ Lit Nil
---             _ <- pure $! evalK (Lit (BoolLit False)) (\e env -> do 
---                 writeIORef tref e >> return emptyEnvironment) emptyEnvironment 
---             result <- readIORef tref
---             result `shouldBe` Lit (BoolLit False)
+totalEmptyEnvironment = Environment EmptyEnv EmptyEnv Nothing
+
+spec :: Spec
+spec = do
+  describe "Testing the EvalK Evaluation" $ do
+    it "Unary negation converts number to bool" $ do
+     let prog = parseProgram "var r = !(5.0);"
+     (Environment _ resultGlobalEnv _) <- interpretK prog return totalEmptyEnvironment
+     resultGlobalEnv `envShouldBe` (Lit (BoolLit False))
+
+--Checks the "r" variable for the result.
+--This is a kludge around evalK only returning an env
+--Expects the test result to be a flat env with no globals and no continuation
+resulte `envShouldBe` expectede = resulte `shouldBe` Env (M.fromList [("r",expectede)]) EmptyEnv
